6c28a6f2908d9517e2332c41c803e24af109ae45
Han Young <hanyoung@protonmail.com>
2020-08-26 14:08:39 +0800

    don't init GUI if running in daemon mode
    -this will save energy and memory consumption

--- src/main.cpp
+++ src/main.cpp
@@ -52,16 +52,61 @@ QCommandLineParser *createParser()
     parser->addOption(QCommandLineOption(QStringLiteral("daemon"), i18n("Run in background mode")));
     parser->addHelpOption();
     return parser;
-}
+};
+
+QObject* initGui(AlarmModel* alarmModel){
+    static bool isInited = false;
+    static QQmlApplicationEngine *engine;
+
+    if(!isInited){
+        engine = new QQmlApplicationEngine();
+
+        // initialize models
+        auto *timeZoneModel = new TimeZoneSelectorModel();
+
+        auto *timeZoneViewModel = new QSortFilterProxyModel();
+        timeZoneViewModel->setFilterFixedString("true");
+        timeZoneViewModel->setSourceModel(timeZoneModel);
+        timeZoneViewModel->setFilterRole(TimeZoneSelectorModel::ShownRole);
+
+        auto *timeZoneFilterModel = new TimeZoneFilterModel(timeZoneModel);
+        auto *utilModel = new UtilModel();
+        auto *stopwatchTimer = new StopwatchTimer();
+        auto *kclockFormat = new KclockFormat();
+        auto *weekModel = new WeekModel();
+        TimerModel::init();
+
+        engine->rootContext()->setContextObject(new KLocalizedContext(engine));
+        KClockSettings settings;
+
+        // register QML types
+        qmlRegisterType<Alarm>("kclock", 1, 0, "Alarm");
+        qmlRegisterType<Timer>("kclock", 1, 0, "Timer");
+
+        // models
+        engine->rootContext()->setContextProperty("timeZoneShowModel", timeZoneViewModel);
+        engine->rootContext()->setContextProperty("timeZoneFilterModel", timeZoneFilterModel);
+        engine->rootContext()->setContextProperty("alarmModel", alarmModel);
+        engine->rootContext()->setContextProperty("timerModel", TimerModel::inst());
+        engine->rootContext()->setContextProperty("settingsModel", &settings);
+        engine->rootContext()->setContextProperty("utilModel", utilModel);
+        engine->rootContext()->setContextProperty("stopwatchTimer", stopwatchTimer);
+        engine->rootContext()->setContextProperty("alarmPlayer", &AlarmPlayer::instance());
+        engine->rootContext()->setContextProperty("kclockFormat", kclockFormat);
+        engine->rootContext()->setContextProperty("weekModel", weekModel);
+        engine->load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+
+        isInited = true;
+    }
+    return engine->rootObjects().first();
+};
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QQmlDebuggingEnabler enabler;
-    QQmlApplicationEngine engine;
 
     KLocalizedString::setApplicationDomain("kclock");
-    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
     KAboutData aboutData("kclock", "Clock", "0.1", "Simple clock made in Kirigami", KAboutLicense::GPL, i18n("© 2020 KDE Community"));
     aboutData.addAuthor(i18n("Devin Lin"), QString(), QStringLiteral("espidev@gmail.com"));
     KAboutData::setApplicationData(aboutData);
@@ -72,57 +117,30 @@ int main(int argc, char *argv[])
     app.setQuitOnLastWindowClosed(false);
 
     // initialize models
-    auto *timeZoneModel = new TimeZoneSelectorModel();
-
-    auto *timeZoneViewModel = new QSortFilterProxyModel();
-    timeZoneViewModel->setFilterFixedString("true");
-    timeZoneViewModel->setSourceModel(timeZoneModel);
-    timeZoneViewModel->setFilterRole(TimeZoneSelectorModel::ShownRole);
-
-    auto *timeZoneFilterModel = new TimeZoneFilterModel(timeZoneModel);
     auto *alarmModel = new AlarmModel();
-    auto *utilModel = new UtilModel();
-    auto *stopwatchTimer = new StopwatchTimer();
-    auto *kclockFormat = new KclockFormat();
-    auto *weekModel = new WeekModel();
-    TimerModel::init();
-    KClockSettings settings;
-
-    // register QML types
-    qmlRegisterType<Alarm>("kclock", 1, 0, "Alarm");
-    qmlRegisterType<Timer>("kclock", 1, 0, "Timer");
-
-    // models
-    engine.rootContext()->setContextProperty("timeZoneShowModel", timeZoneViewModel);
-    engine.rootContext()->setContextProperty("timeZoneFilterModel", timeZoneFilterModel);
-    engine.rootContext()->setContextProperty("alarmModel", alarmModel);
-    engine.rootContext()->setContextProperty("timerModel", TimerModel::inst());
-    engine.rootContext()->setContextProperty("settingsModel", &settings);
-    engine.rootContext()->setContextProperty("utilModel", utilModel);
-    engine.rootContext()->setContextProperty("stopwatchTimer", stopwatchTimer);
-    engine.rootContext()->setContextProperty("alarmPlayer", &AlarmPlayer::instance());
-    engine.rootContext()->setContextProperty("kclockFormat", kclockFormat);
-    engine.rootContext()->setContextProperty("weekModel", weekModel);
-    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     {
         QScopedPointer<QCommandLineParser> parser(createParser());
         parser->process(app);
-        QObject *rootObject = engine.rootObjects().first();
         if (parser->isSet(QStringLiteral("page"))) {
-            QMetaObject::invokeMethod(rootObject, "switchToPage", Q_ARG(QVariant, parser->value("page")));
+            QMetaObject::invokeMethod(initGui(alarmModel), "switchToPage", Q_ARG(QVariant, parser->value("page")));
         }
         if (parser->isSet(QStringLiteral("no-powerdevil"))) {
             qDebug() << "No PowerDevil option set, disabling PowerDevil usage";
             alarmModel->setUsePowerDevil(false);
         }
         if (!parser->isSet(QStringLiteral("daemon"))) {
-            QMetaObject::invokeMethod(rootObject, "show");
+            QMetaObject::invokeMethod(initGui(alarmModel), "show");
+
+            QObject::connect(&service, &KDBusService::activateRequested, initGui(alarmModel), [=](const QStringList &arguments, const QString &workingDirectory) {
+                Q_UNUSED(workingDirectory)
+                QMetaObject::invokeMethod(initGui(alarmModel), "show");
+            });
+        } else {
+            QObject::connect(&service, &KDBusService::activateRequested, [=] {
+                QMetaObject::invokeMethod(initGui(alarmModel), "show");
+            });
         }
-        QObject::connect(&service, &KDBusService::activateRequested, rootObject, [=](const QStringList &arguments, const QString &workingDirectory) {
-            Q_UNUSED(workingDirectory)
-            QMetaObject::invokeMethod(rootObject, "show");
-        });
     }
 
     // start alarm polling
