39219d67a939c6b0dae6aa8e3887738d8d3c914c
Han Young <hanyoung@protonmail.com>
2020-08-30 19:39:20 +0800

    add KClockdSettings to kclockd

--- src/kclockd/main.cpp
+++ src/kclockd/main.cpp
@@ -31,7 +31,8 @@
 #include <KDBusService>
 
 #include "alarmmodel.h"
-
+#include "kclockdadaptor.h"
+#include "kclockdsettings.h"
 QCommandLineParser *createParser()
 {
     QCommandLineParser *parser = new QCommandLineParser;
@@ -47,15 +48,18 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain("kclockd");
-    KAboutData aboutData(QStringLiteral("kclockd"),QStringLiteral("KClock daemon"), QStringLiteral("0.1"), QStringLiteral("KClock daemon"), KAboutLicense::GPL, i18n("© 2020 KDE Community"));
-    aboutData.addAuthor(i18n("Devin Lin"), QString(), QStringLiteral("espidev@gmail.com"));
-    aboutData.addAuthor(i18n("Han Young"), QString(), QStringLiteral("hanyoung@protonmail.com"));
+    KAboutData aboutData(QStringLiteral("kclockd"), QStringLiteral("KClock daemon"), QStringLiteral("0.1"), QStringLiteral("KClock daemon"), KAboutLicense::GPL, i18n("© 2020 KDE Community"));
+    aboutData.addAuthor(i18n("Devin Lin"), QStringLiteral(), QStringLiteral("espidev@gmail.com"));
+    aboutData.addAuthor(i18n("Han Young"), QStringLiteral(), QStringLiteral("hanyoung@protonmail.com"));
     KAboutData::setApplicationData(aboutData);
 
     // only allow one instance
     KDBusService service(KDBusService::Unique);
 
     // initialize models
+    auto *settings = new KClockSettings();
+    new KClockSettingsAdaptor(settings);
+    QDBusConnection::sessionBus().registerObject("/Settings", settings);
 
     // start alarm polling
     AlarmModel::instance()->configureWakeups();
