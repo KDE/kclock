208f1f4834a0b127e786486e8848f0e1f710feea
Han Young <hanyoung@protonmail.com>
2020-09-05 15:31:00 +0800

    add license header

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2020   Han Young <hanyoung@protonmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <KLocalizedString>
 #include <KNotification>
 
@@ -77,7 +96,7 @@ void Timer::setRunning(bool running)
 
     if (m_running) {
         m_hasElapsed = QDateTime::currentSecsSinceEpoch() - m_startTime;
-        
+
         if (m_cookie > 0) {
             Utilities::instance().clearWakeup(m_cookie);
             m_cookie = -1;
@@ -86,7 +105,7 @@ void Timer::setRunning(bool running)
         if (m_hasElapsed == m_length) { // reset elapsed if the timer was already finished
             m_hasElapsed = 0;
         }
-        
+
         m_startTime = QDateTime::currentSecsSinceEpoch() - m_hasElapsed;
         m_cookie = Utilities::instance().scheduleWakeup(m_startTime + m_length);
     }
