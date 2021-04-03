8b19690d710c1ab874b8f17560b0596849e6dbb4
Ludovic Jozeau <federandink@gmail.com>
2021-04-03 18:29:46 +0200

    add *.hpp to clang-format target

--- src/kclock/kclock_algorithm.hpp
+++ src/kclock/kclock_algorithm.hpp
@@ -2,7 +2,10 @@
 
 namespace KClock
 {
-template<class C> int insert_index(const typename C::value_type &obj, const C &container, bool (*less_than)(const typename C::value_type &left, const typename C::value_type &right))
+template<class C>
+int insert_index(const typename C::value_type &obj,
+                 const C &container,
+                 bool (*less_than)(const typename C::value_type &left, const typename C::value_type &right))
 {
     int index = 0;
     for (auto element : container) {
